76b064e2ba575a88917e438f2cea25b9bbba5203
German Wt <germanwt@gmail.com>
2018-11-27 20:26:10 +0100

    commit variable

--- JavaScript/JS_Tot/02_Variables.js
+++ JavaScript/JS_Tot/02_Variables.js
@@ -1,2 +1,46 @@
 // Variables 
+/*
+var pais = "España"; 
+var continente = "Europa"; 
+var edad= 34; 
+var pais_y_continente= pais + ' ' + continente
 
+let prueba= 'hola'; 
+
+pais= 'Mexico';
+continente='latinoamerica'; 
+
+console.log (pais, continente, edad); 
+console.log (pais_y_continente); 
+console.log(prueba); 
+
+
+// Prueba con var variable global
+var numero = 40;
+console.log(numero);
+
+if (true){
+    var numero=50; 
+    console.log(numero);
+}
+console.log(numero); 
+
+// prueba con let variable local 
+var texto= 'curso JS udemy';
+console.log(texto); 
+
+if (true){
+    let texto= 'curso uno let';
+    console.log(texto); 
+}
+console.log(texto); */
+
+// Constantes
+
+var web='https:// udemy.com';
+const ip = '192.88.0.12'; 
+
+web = 'https:// gmail.com';
+// ip =  '232323'; 
+
+console.log(web, ip); 
