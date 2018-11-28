79d759244c5deba0f1c2c6f4a3786f257592d1e7
German Wt <germanwt@gmail.com>
2018-11-28 10:28:51 +0100

    Commit Operadores

---
+++ JavaScript/JS_Tot/03_Operadores.js
@@ -0,0 +1,19 @@
+// Operadores y tipos de datos 
+
+var nume1 = 7; 
+var nume2= 12; 
+var operacion = nume1 +  nume2; 
+
+// alert(" el resultado es: " + operacion); 
+
+var texto1= 'Hola que tal '; 
+var nume3= 44; 
+var ver_Falso= "false";  
+
+var num5 = '33';
+
+//console.log (Number (num5) + nume3); 
+//console.log(parseInt(num5) + nume3); 
+// console.log (String(num5) + 4); 
+// console.log (texto1 + nume3); 
+console.log(typeof num5); 
