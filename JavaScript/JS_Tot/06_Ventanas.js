980de5e3f21514c7deba61cc4c6a8a060b335d0d
German Wt <germanwt@gmail.com>
2018-11-29 10:50:32 +0100

    Commit_Ventanas

---
+++ JavaScript/JS_Tot/06_Ventanas.js
@@ -0,0 +1,18 @@
+'use strict'
+
+// Alert 
+
+//alert('esta es mi alerta'); 
+
+// confirmación 
+
+//var confirmación= confirm('esta seguro de continuar'); 
+//console.log(confirmación);
+
+// ingreso datos
+
+var result= prompt ('que edad tienes', 18);
+console.log(result); 
+ 
+
+
