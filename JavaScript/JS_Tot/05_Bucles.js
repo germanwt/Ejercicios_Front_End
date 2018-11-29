cfa68a2b1695b29cdd8b0a30acbd10c997097ffc
German Wt <germanwt@gmail.com>
2018-11-29 10:39:42 +0100

    Commit Bucles

---
+++ JavaScript/JS_Tot/05_Bucles.js
@@ -0,0 +1,32 @@
+'use strict'
+// for 
+
+/*
+var num= 100; 
+
+for(var i=0; i<= num; i++){
+    console.log('vamos por numero: '+i);
+
+        debugger;
+}
+
+
+// While 
+
+var year= 2018; 
+
+while (year != 1991) {
+    // ejecuta esto 
+    console.log('estamos en el año: '+year);
+
+        year--; 
+}
+/*
+// Do While 
+
+var year = 30; 
+
+do { // bloque de condiciones primero 
+    alert('solo cuando sea diferente a 20');
+    year --; 
+} while (year > 25) // igual la condicion */
